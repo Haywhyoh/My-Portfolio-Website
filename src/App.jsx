fc48f4f5504ab51dab30c176a9822f9451cac4b9
haywhyoh <adefeyisayo998@gmail.com>
2023-04-17 18:31:42 +0100

    cretaed the home and skill section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import hand from './assets/images/hand.png';
+import myPic from './assets/images/myPic.png';
+import picBg from './assets/images/7.png';
+import Service from './components/Service';
+import service_list from './data/services.json';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <div className=' h-fit w-screen bg-[url("./src/assets/images/banner.jpeg")] font-body bg-cover' >
+      <section className='text-white'>
+        <div className='container'>
+          <h2 className='text-4xl font-bold flex gap-x-3'>Hi <img className='w-10' src={hand} /> I'm Samuel</h2>
+          <div className='text-2xl'>FullStack Software Engineer</div>
+          <div><button className='bg-dark_blue px-8 py-4 rounded-3xl font-bold'>My Resume</button></div>
+        </div>
+        <div className='container'>
+        <img src={myPic} className='absolute w-80 pt-10'/>
+        <img className=' w-96 h-80 ' src={picBg} />
+        </div>
+      </section>
+      <section className='container text-center  bg-layout_black'>
+        <div><h2 className='sub_title font-bold'>My Expertise</h2></div>
+        <div><h2 className=' font-bold text-4xl text-white'>What I Do</h2></div>
+        <div className='flex gap-2 justify-center '>
+          <div className=' w-3 h-3 border-b-4 border-red '></div>
+          <div className=' w-20 h-3 border-b-4 border-red '></div>
+        </div>
+        <div>
+          <div>
+            {service_list.map(service => (
+              <Service
+                key={service.id}
+                {...service}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className='container text-center py-10'>
+      <div><h2 className='sub_title font-bold'>Recent Work</h2></div>
+      <div className='text-white text-6xl font-bold py-5'> Check out my portfolio and give a feedback</div>
+      <div className='text-white text-6xl font-bold py-5'> 10 <br></br> <span className='text-2xl'>Completed Projects</span></div>
+      <div className='text-white text-6xl font-bold py-5'> 150+ <br></br><span className='text-2xl'>Tech Articles</span></div>
+      
+      </section>
+      <section className='mx-4'>
+        <div className='border-white w-full border-b-2'></div>
+        <div className='text-white text-6xl font-bold py-5 text-center py-10' > 98% <br></br><span className='text-2xl'>Positive Rating</span></div>
+      </section>
 
-export default App
+    </div>
+  );
+}
+export default App;
