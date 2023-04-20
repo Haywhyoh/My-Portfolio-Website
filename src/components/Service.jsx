87c300171111ad151fd55a2644b9591035b90088
haywhyoh <adefeyisayo998@gmail.com>
2023-04-20 15:22:27 +0100

    created service component

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,13 +1,14 @@
 
 export default function Service (props) {
   return (
-    <div className="text-white py-5 flex flex-col justify-center" >
-      <div className="w-20 flex justify-center mx-auto">
-        <img src={`../src/assets/images/${props.imageUrl}.png`} alt="" />
+    <div className='text-white py-5 flex flex-col justify-center'>
+      <div className='w-20 flex justify-center mx-auto'>
+        <img src={`../src/assets/images/${props.imageUrl}.png`} alt='' />
       </div>
 
-      <div>
-        <h2 className="text-3xl font-bold py-2">{props.service}</h2></div>
+      <div className="">
+        <h2 className='text-3xl font-bold py-2'>{props.service}</h2>
+      </div>
       <div>{props.content}</div>
     </div>
   );
