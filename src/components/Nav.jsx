eb1fe45f1132abe8509be8780494121a8ebb66ac
haywhyoh <adefeyisayo998@gmail.com>
2023-04-22 21:18:51 +0100

    created te mobile nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,41 @@
 import { FiMenu } from 'react-icons/fi';
+import { RiCloseFill } from 'react-icons/ri';
 export default function Nav () {
+  function hideNav () {
+    const mobileNav = document.querySelector('#mobileNav');
+    const list = mobileNav.classList;
+    list.add('hidden');
+    list.remove('block');
+  }
+
+  function showNav () {
+    const mobileNav = document.querySelector('#mobileNav');
+    const list = mobileNav.classList;
+    list.remove('hidden');
+    list.add('block');
+  }
   return (
     <div className='text-white'>
-      <div className='bg-black_bg p-4 flex justify-between items-center fixed w-full z-50'>
-        <div className='text-4xl'><FiMenu /> </div>
+      <div className='bg-black_bg p-4  flex justify-between items-center fixed w-full z-30'>
+        <div className='text-4xl' onClick={showNav}><FiMenu /> </div>
         <div><img className=' w-20 text-2xl' src='../src/assets/images/logo.svg' /></div>
+      </div>
+      <div className='fixed z-50 w-full h-screen bg-layout_black2 hidden' id='mobileNav' onClick={hideNav}>
+        <div className='w-4/5 bg-white h-full text-black_text font-bold p-5 ps-8 '>
+          <div className='flex  justify-between'>
+            <div className=''><img className='text-2xl' src='../src/assets/images/logo.svg' /></div>
+            <div className='text-2xl z-50 border-2 border-layout_black text-dark_blue font-bold h-fit rounded-full p-1'><RiCloseFill /> </div>
+          </div>
+          <div>
+            <div className='py-5 border-b border-b-layout_black'>Home</div>
+            <div className='py-5 border-b border-b-layout_black'>Services</div>
+            <div className='py-5 border-b border-b-layout_black'>Portfolio</div>
+            <div className='py-5 border-b border-b-layout_black'>About</div>
+            <div className='py-5 border-b border-b-layout_black'>Resume</div>
+            <div className='py-5 border-b border-b-layout_black'>Contact</div>
+
+          </div>
+        </div>
       </div>
 
     </div>
