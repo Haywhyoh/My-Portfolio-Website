c7a9a0d4b524a101c4b429bb2265127146e1d0f4
haywhyoh <adefeyisayo998@gmail.com>
2023-04-23 20:45:23 +0100

    made the service card responsive for tablets

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,12 +1,12 @@
 
 export default function Service (props) {
   return (
-    <div className='text-white py-5 flex flex-col justify-center'>
-      <div className='w-20 flex justify-center mx-auto'>
+    <div className='text-white py-5 flex flex-col justify-center md:bg-black_bg md:p-8 md:text-left md:w-80'>
+      <div className='w-20 flex justify-center mx-auto md:mx-0'>
         <img src={`../src/assets/images/${props.imageUrl}.png`} alt='' />
       </div>
 
-      <div className="">
+      <div className=''>
         <h2 className='text-3xl font-bold py-2'>{props.service}</h2>
       </div>
       <div>{props.content}</div>
