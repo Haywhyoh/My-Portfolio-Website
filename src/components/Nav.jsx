c98948d0125986601a2e0e3e05c4d0c0e1ea9979
haywhyoh <adefeyisayo998@gmail.com>
2023-04-23 20:44:27 +0100

    created the mobile responsive nav for tab screen

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,16 +3,27 @@ import { RiCloseFill } from 'react-icons/ri';
 export default function Nav () {
   function hideNav () {
     const mobileNav = document.querySelector('#mobileNav');
+    const navList = document.querySelector('#navList');
+
     const list = mobileNav.classList;
+    const navListClass = navList.classList;
+
     list.add('hidden');
     list.remove('block');
+    navListClass.add('animate__slideOutLeft');
+    navListClass.remove('animate__slideInLeft');
   }
 
   function showNav () {
     const mobileNav = document.querySelector('#mobileNav');
+    const navList = document.querySelector('#navList');
+
     const list = mobileNav.classList;
+    const navListClass = navList.classList;
     list.remove('hidden');
     list.add('block');
+    navListClass.add('animate__slideInLeft');
+    navListClass.remove('animate__slideOutLeft');
   }
   return (
     <div className='text-white'>
@@ -21,7 +32,7 @@ export default function Nav () {
         <div><img className=' w-20 text-2xl' src='../src/assets/images/logo.svg' /></div>
       </div>
       <div className='fixed z-50 w-full h-screen bg-layout_black2 hidden' id='mobileNav' onClick={hideNav}>
-        <div className='w-4/5 bg-white h-full text-black_text font-bold p-5 ps-8 '>
+        <div className='w-4/5 bg-white h-full text-black_text font-bold p-5 ps-8 animate__animated' id='navList'>
           <div className='flex  justify-between'>
             <div className=''><img className='text-2xl' src='../src/assets/images/logo.svg' /></div>
             <div className='text-2xl z-50 border-2 border-layout_black text-dark_blue font-bold h-fit rounded-full p-1'><RiCloseFill /> </div>
