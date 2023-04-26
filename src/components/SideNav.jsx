243b2973b948c3c60587e2cf42fb3fcac0bc3e38
haywhyoh <adefeyisayo998@gmail.com>
2023-04-26 09:57:46 +0100

    added the smooth scroll animation

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import { RiHome4Line } from 'react-icons/ri';
 import { MdOutlineDesignServices } from 'react-icons/md';
 import { SlBriefcase } from 'react-icons/sl';
@@ -16,16 +17,34 @@ export default function SideNav () {
   const [icon, setIcon] = useState(initialState);
 
   return (
-    <div className='hidden lg:w-28 xl:w-36 fixed z-40  bg-dark_blue lg:block'>
+    <div className='hidden h-screen lg:w-28 xl:w-36 fixed z-40  bg-dark_blue lg:block'>
       <div className='flex flex-col h-screen justify-between my-auto gap-y-7'>
         <div className='w-full py-4'><img src='./src/assets/images/logo.svg' className='w-full h-20' /></div>
         <div className='text-4xl flex flex-col justify-center mx-auto my-4'>
-          <div className='px-8 py-4 border-y-white border-b-2 border-t-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, home: true }))}> {icon.home ? 'Home' : <RiHome4Line className='text-4xl' />}</div>
-          <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, services: true }))}> {icon.services ? 'Services' : <MdOutlineDesignServices className='text-4xl' />}</div>
-          <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, portfolio: true }))}>{icon.portfolio ? 'Portfolio' : <SlBriefcase className='text-4xl' />}</div>
-          <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, about: true }))}>{icon.about ? 'About' : <SiAboutdotme className='text-4xl' />}</div>
-          <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, resume: true }))}>{icon.resume ? 'Resume' : <BsFileEarmarkRichtext className='text-4xl' />}</div>
-          <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, contact: true }))}>{icon.contact ? 'Contact' : <BsChatText className='text-4xl' />} </div>
+          <Link activeClass='active' to='home' spy smooth offset={-70} duration={200}>
+            <div className='px-8 py-4 border-y-white border-b-2 border-t-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, home: true }))}> {icon.home ? 'Home' : <RiHome4Line className='text-4xl' />}</div>
+
+          </Link>
+          <Link activeClass='active' to='services' spy smooth offset={-70} duration={200}>
+            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, services: true }))}> {icon.services ? 'Services' : <MdOutlineDesignServices className='text-4xl' />}</div>
+
+          </Link>
+          <Link activeClass='active' to='portfolio' spy smooth offset={100} duration={200}>
+            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, portfolio: true }))}>{icon.portfolio ? 'Portfolio' : <SlBriefcase className='text-4xl' />}</div>
+
+          </Link>
+          <Link activeClass='active' to='about' spy smooth offset={-70} duration={200}>
+            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, about: true }))}>{icon.about ? 'About' : <SiAboutdotme className='text-4xl' />}</div>
+
+          </Link>
+          <Link activeClass='active' to='skillMenu' spy smooth offset={-70} duration={200}>
+            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, resume: true }))}>{icon.resume ? 'Resume' : <BsFileEarmarkRichtext className='text-4xl' />}</div>
+
+          </Link>
+          <Link activeClass='active' to='contact' spy smooth offset={-70} duration={200}>
+
+            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, contact: true }))}>{icon.contact ? 'Contact' : <BsChatText className='text-4xl' />} </div>
+          </Link>
         </div>
         <div className='flex justify-center mb-10'>
           <BsPlay className='text-6xl p-2 bg-blue rounded-full mb-8' />
