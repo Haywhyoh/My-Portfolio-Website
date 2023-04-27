b5387949442dca7f52c78e4c1a4f1e9bce329d67
haywhyoh <adefeyisayo998@gmail.com>
2023-04-27 10:14:14 +0100

    made the experience care more responsve

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -20,13 +20,13 @@ export default function ProjectCard (props) {
   }
   return (
     <div className='md:w-80 md:relative lg:w-96 xl:w-80'>
-      <div onClick={showDetail}><img className='relative' src={`../src/assets/images/${props.imageUrl}.jpeg`} alt='' /></div>
+      <div onClick={showDetail} onMouseOver={showDetail}><img className='relative' src={`../src/assets/images/${props.imageUrl}.jpeg`} alt='' /></div>
       <div className='relative md:absolute md:bottom-0 text-white text-md bg-dark_blue w-full py-3 px-5 bottom-5 projectDetail flex justify-between invisible'>
         <div>
           <div className='opacity-80 font-bold'>{props.title}</div>
           <div className='opacity-50 font-bold'>{props.category}</div>
         </div>
-        <div>
+        <div className='animate__animated animate__headShake animate__infinite infinite'>
           <button className='bg-red p-4 rounded-full'>
             <HiOutlineArrowNarrowRight />
           </button>
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -20,34 +20,34 @@ export default function SideNav () {
     <div className='hidden h-screen lg:w-28 xl:w-36 fixed z-40  bg-dark_blue lg:block'>
       <div className='flex flex-col h-screen justify-between my-auto gap-y-7'>
         <div className='w-full py-4'><img src='./src/assets/images/logo.svg' className='w-full h-20' /></div>
-        <div className='text-4xl flex flex-col justify-center mx-auto my-4'>
+        <div className='text-4xl flex flex-col justify-center  my-4'>
           <Link activeClass='active' to='home' spy smooth offset={-70} duration={200}>
-            <div className='px-8 py-4 border-y-white border-b-2 border-t-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, home: true }))}> {icon.home ? 'Home' : <RiHome4Line className='text-4xl' />}</div>
+            <div className='py-4 flex justify-center border-y-white border-b-2 border-t-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, home: true }))}> {icon.home ? 'Home' : <RiHome4Line className='text-4xl' />}</div>
 
           </Link>
-          <Link activeClass='active' to='services' spy smooth offset={-70} duration={200}>
-            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, services: true }))}> {icon.services ? 'Services' : <MdOutlineDesignServices className='text-4xl' />}</div>
+          <Link activeClass='active' to='services' spy smooth offset={100} duration={200}>
+            <div className=' flex justify-center  py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, services: true }))}> {icon.services ? 'Services' : <MdOutlineDesignServices className='text-4xl' />}</div>
 
           </Link>
           <Link activeClass='active' to='portfolio' spy smooth offset={100} duration={200}>
-            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, portfolio: true }))}>{icon.portfolio ? 'Portfolio' : <SlBriefcase className='text-4xl' />}</div>
+            <div className=' flex justify-center  py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, portfolio: true }))}>{icon.portfolio ? 'Portfolio' : <SlBriefcase className='text-4xl' />}</div>
 
           </Link>
           <Link activeClass='active' to='about' spy smooth offset={-70} duration={200}>
-            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, about: true }))}>{icon.about ? 'About' : <SiAboutdotme className='text-4xl' />}</div>
+            <div className=' flex justify-center  py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, about: true }))}>{icon.about ? 'About' : <SiAboutdotme className='text-4xl' />}</div>
 
           </Link>
           <Link activeClass='active' to='skillMenu' spy smooth offset={-70} duration={200}>
-            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, resume: true }))}>{icon.resume ? 'Resume' : <BsFileEarmarkRichtext className='text-4xl' />}</div>
+            <div className=' flex justify-center  py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, resume: true }))}>{icon.resume ? 'Resume' : <BsFileEarmarkRichtext className='text-4xl' />}</div>
 
           </Link>
           <Link activeClass='active' to='contact' spy smooth offset={-70} duration={200}>
 
-            <div className='px-8 py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, contact: true }))}>{icon.contact ? 'Contact' : <BsChatText className='text-4xl' />} </div>
+            <div className=' flex justify-center  py-4 border-y-white border-b-2 border-opacity-20 text-xl' onClick={() => setIcon(() => ({ ...initialState, contact: true }))}>{icon.contact ? 'Contact' : <BsChatText className='text-4xl' />} </div>
           </Link>
         </div>
-        <div className='flex justify-center mb-10'>
-          <BsPlay className='text-6xl p-2 bg-blue rounded-full mb-8' />
+        <div className='flex justify-center mb-10 '>
+          <BsPlay className='text-6xl p-2 bg-blue rounded-full mb-8 animate__animated animate__heartBeat animate__infinite infinite' />
         </div>
       </div>
 
