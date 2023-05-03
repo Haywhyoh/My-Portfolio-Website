43db340b817694d276e78a35dad5a2ba255404b0
haywhyoh <adefeyisayo998@gmail.com>
2023-05-03 08:09:31 +0100

    made the services component more responsive

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -3,7 +3,7 @@ export default function Service (props) {
   return (
     <div className=' py-5 flex flex-col justify-center md:bg-black_bg md:p-8 md:text-left md:w-80 lg:bg-layout_black lg:bg-opacity-0 lg:w-72 xl:w-80 xl:mx-auto 2xl:w-96 2xl:p-12 '>
       <div className='w-20 flex justify-center mx-auto md:mx-0'>
-        <img src={`../../public/images/${props.imageUrl}.png`} alt='' />
+        <img src={`/images/${props.imageUrl}.png`} alt='' />
       </div>
 
       <div className=''>
