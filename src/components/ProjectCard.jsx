7e9563709e3411ed0c66f916b6b319af010083e4
haywhyoh <adefeyisayo998@gmail.com>
2023-05-05 14:31:03 +0100

    styled the modal box

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,8 @@
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 import { Modal } from 'react-overlays';
 import { useState } from 'react';
+import { BsGithub } from 'react-icons/bs';
+import { RiCloseCircleFill } from 'react-icons/ri';
 
 export default function ProjectCard (props) {
   function showDetail () {
@@ -39,49 +41,72 @@ export default function ProjectCard (props) {
   };
   const renderBackdrop = (props) => <div className='backdrop' {...props} />;
   return (
-    <div className='md:w-80 md:relative lg:w-96 xl:w-80 '>
+    <div className='md:w-80 md:relative lg:w-96 xl:w-80'>
       <Modal
-        className='justify-center mx-auto flex  bg-layout_black2'
+        className='justify-center mx-auto flex  bg-layout_black2 md:py-0 h-screen'
         show={showModal}
         onHide={handleClose}
         renderBackdrop={renderBackdrop}
-
       >
-        <div className=' w-2/3 h-screen bg-dark_blue text-white'>
-          <button onClick={handleClose}>Close</button>
+        <div className=' w-2/3 h-screen bg-dark_blue text-white overflow-auto relative'>
+          <button onClick={handleClose} className='p-4 text-4xl absolute right-0 top-2'> <RiCloseCircleFill className='text-red ' /></button>
           <div>
             <img src='/images/project_bg.jpeg' alt='' />
           </div>
-          <div className='flex flex-col bg-black_bg lg-flex-row justify-center gap-3 items-center p-8'>
-            <div>
+          <div className='flex flex-col bg-black_bg lg:flex-row lg:gap-x-10 justify-center text-left gap-3 p-8'>
+            <div className='lg:w-1/2'>
               <div>
-                <div className='font-bold text-xl'>CLIENT</div>
-                <div>Personal</div>
+                <div className='py-3 font-bold text-lg'>CLIENT</div>
+                <div className='opacity-50'>Personal</div>
               </div>
               <div>
-                <div className='font-bold text-xl'>DATE</div>
-                <div>01, October, 2022</div>
+                <div className='py-3 font-bold text-lg'>DATE</div>
+                <div className='opacity-50'>01, October, 2022</div>
               </div>
 
             </div>
-            <div>
+            <div className='lg:w-1/2'>
               <div>
-                <div className='font-bold text-xl'>PROJECT TYPE</div>
-                <div>API development</div>
+                <div className='py-3 font-bold text-lg'>PROJECT TYPE</div>
+                <div className='opacity-50'>API development</div>
               </div>
               <div>
-                <div className='font-bold text-xl'>LINKS</div>
-                <div>Github <div>github.com</div></div>
-                <div>Live: <div>writeabout.com</div></div>
+                <div className='font-bold text-lg'>LINKS</div>
+                <div className='py-3 text-opacity-20'> <div><BsGithub /> </div></div>
+                <div className='opacity-50'>Live: <div>writeabout.com</div></div>
               </div>
-              className='font-bold text-xl'
             </div>
 
           </div>
-          <div>
-            <div>Project Description</div>
-            <p>anglngsd</p>
-            <div>Technologies Used</div>
+          <div className='p-4 lg:p-8'>
+            <div className='text-3xl text-center lg:text-left font-bold'>Project Description</div>
+            <p className='py-4 opacity-50'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec porttitor, neque et tempor blandit, ex dui viverra urna, vel aliquam metus lacus non ipsum.
+              Vestibulum elementum magna quis auctor viverra. Quisque aliquam massa sapien. Donec sit amet gravida sapien.
+              Nunc sit amet eleifend tellus. Nunc facilisis et dui at dapibus.
+              Nullam sed lacus egestas, rutrum purus quis, volutpat lectus.
+
+              Aliquam scelerisque quam sit amet tortor consequat, ac rutrum lectus dictum.
+              Aliquam nibh ante, aliquet ac justo vitae, tempus vestibulum dui. Sed eu rutrum nulla.
+              Donec et nibh ex. Maecenas luctus turpis ac dui convallis feugiat.
+              Nullam semper dui dui, pharetra ullamcorper ante bibendum at.
+              Nullam a sapien vehicula, scelerisque augue eget, lobortis quam.
+              Maecenas vulputate laoreet dui eu pellentesque.
+              Ut ullamcorper orci in iaculis varius.
+              Mauris congue nisl nec erat euismod malesuada.
+              Cras finibus sem ac leo imperdiet vestibulum.
+              Vestibulum id lorem vel nibh rutrum luctus id nec tortor. Sed id lacus ut purus tristique efficitur quis quis odio.
+            </p>
+            <div className='text-3xl font-bold text-center lg:text-left'>Technologies Used</div>
+            <div className='flex py-1 justify-center lg:justify-start opacity-60'>
+              <ul className=' list-disc p-8'>
+                <li>NodeJS</li>
+                <li>TypeScript</li>
+                <li>Python</li>
+                <li>MongoDb</li>
+                <li>Flask</li>
+              </ul>
+            </div>
             <div>Architecture</div>
           </div>
         </div>
